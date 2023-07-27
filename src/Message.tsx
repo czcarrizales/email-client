751ecc6e84c155330f1c6d4544d41c23629ea882
Chazz Carrizales <czcarrizales@yahoo.com>
2023-07-27 00:33:37 -0700

    adding routes and interactivity (Message)

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,13 +1,35 @@
 import React from 'react'
+import {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
 import './Message.css'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Message = () => {
+  const navigate = useNavigate()
+  const [isFavorited, setIsFavorited] = useState(false)
+  const [color, setColor] = useState('#44404F')
+  const viewMessage = () => {
+    navigate('/message-view')
+  }
+  const goToProfile = () => {
+    navigate('/profile')
+  }
+  const handleFavorite = () => {
+    if (isFavorited === false) {
+      setIsFavorited(true)
+      setColor('yellow')
+    } else {
+      setIsFavorited(false)
+      setColor('#44404F')
+    }
+  }
   return (
     <div id="message-container" className="row">
-      <div id='message-profile' className="col-3 d-flex align-items-center justify-content-center">Profile Picture</div>
-      <div className="col-6">
+      <div id='message-profile' className="col-3 d-flex align-items-center justify-content-center" onClick={goToProfile}>
+        <p className='profile-picture'></p>
+      </div>
+      <div className="col-6" onClick={viewMessage}>
         <div className="row">
           <div id='message-name' className="col-12">Chazz Carrizales</div>
           <div id='message-title' className="col-12">Yo, Let's Hang Out!</div>
@@ -17,7 +39,7 @@ const Message = () => {
       <div className="col-3 d-flex align-items-center justify-content-end">
         <div className="row">
           <div className="col-12 text-end">March 17</div>
-          <div className="col-12 text-end"><FontAwesomeIcon id='star' icon={faStar} /></div>
+          <div className="col-12 text-end"><FontAwesomeIcon id='star' icon={faStar} onClick={handleFavorite} style={{color}} /></div>
         </div>
       </div>
     </div>
