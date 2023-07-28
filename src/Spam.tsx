463c628b96ab620b27b57b0e136b0c95119287cd
Chazz Carrizales <czcarrizales@yahoo.com>
2023-07-27 17:59:36 -0700

    Spam component functionality

--- src/Spam.tsx
+++ src/Spam.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { InputContext } from './InputContext'
+import Message from './Message'
+import { MessageContext } from './MessageContext'
 
 const Spam = () => {
+    const {messages} = useContext(MessageContext)
+    const spamMessages = messages.filter((message) => message.spam)
+    const {inputValue} = useContext(InputContext)
+    const [filteredMessages, setFilteredMessages] = useState([])
+
+    useEffect(() => {
+        const filtered = spamMessages.filter((message) => {
+            const first = message.name.first
+            return first.toLowerCase().includes(inputValue.toLowerCase())
+        })
+        setFilteredMessages(filtered)
+        console.log(filteredMessages, inputValue)
+    }, [messages, inputValue])
+
+
+      const displayMessages = () => {
+        return filteredMessages.map((message) => (<Message firstName={message.name.first} lastName={message.name.last} picture={message.picture.medium} id={message.login.uuid} />))
+      }
   return (
-    <div>Spam</div>
+    <div>
+        {displayMessages()}
+    </div>
   )
 }
 
