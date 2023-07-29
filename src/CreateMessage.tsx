8fd99ea188fb3a7f49d3ec6b8cdab26c7a1c91c3
Chazz Carrizales <czcarrizales@yahoo.com>
2023-07-28 18:45:40 -0700

    add navigation for createMessage

--- src/CreateMessage.tsx
+++ src/CreateMessage.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './CreateMessage.css'
+import { MessageContext } from './MessageContext'
+import { useNavigate, useParams } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 const CreateMessage = () => {
+    const navigate = useNavigate()
+    const {messages, sentMessages, setSentMessages} = useContext(MessageContext)
+    const {id} = useParams()
+    const user = messages.find((message) => message.login.uuid === id)
+    const inputValue = user ? user.email : null
+    const onSubmit = (e) => {
+        e.preventDefault()
+        toast.success('Email sent!', {
+            position: "bottom-left",
+            autoClose: 3000,
+            theme: "dark",
+          })
+          setSentMessages([...sentMessages, 'a sent email'])
+        navigate('/')
+        console.log('submit')
+    }
   return (
     <div className="row">
         <div className="col">
         <form action="">
             <div id='create-message-to'>
-            <input className='form-control create-message-input' type="text" placeholder='To:' />
+            <input className='form-control create-message-input' type="text" placeholder='To:' value={inputValue || ''} />
             </div>
             <div id='create-message-subject'>
             <input className='form-control create-message-input' type="text" placeholder='Subject Title' />
@@ -17,7 +36,7 @@ const CreateMessage = () => {
             </div>
             
             <div id='create-message-submit'>
-            <input className='btn btn-primary btn-block create-message-input' type="submit" />
+            <input className='btn btn-primary btn-block create-message-input' type="submit" onClick={onSubmit} />
             </div>
             
         </form>
