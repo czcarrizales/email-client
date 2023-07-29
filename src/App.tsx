d7ccb49bacd8338ab27e1706b57c3dc388150ce9
Chazz Carrizales <czcarrizales@yahoo.com>
2023-07-28 18:45:01 -0700

    imports and correct rows and columns for sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link, useLocation, useParams, matchPath, useMatch } from 'react-router-dom'
 import './App.css'
 import Message from './Message'
 import Navbar from './Navbar'
@@ -6,40 +6,53 @@ import MessageView from './MessageView'
 import Profile from './Profile'
 import Inbox from './Inbox'
 import CreateMessage from './CreateMessage'
+import { MessageProvider } from './MessageContext'
 import { faMessage } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { InputProvider } from './InputContext'
+import Favorites from './Favorites'
+import Spam from './Spam'
+import Trash from './Trash'
+import Sent from './Sent'
+import CustomButton from './CustomButton'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import Sidebar from './Sidebar'
 
 function App() {
 
-  const getMessages = () => {
-    return (
-      <>
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-      </>
-    )
-  }
+  const match = useMatch('/message-view/:id');
+  const id = match?.params?.id || null
 
   return (
-    <BrowserRouter>
-      <div className='container'>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Inbox />} />
-          <Route path='/message-view' element={<MessageView />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/create-message' element={<CreateMessage />} />
-        </Routes>
-        <Link id="btn-fixed" to='/create-message'><FontAwesomeIcon icon={faMessage} size='2x' /></Link>
+
+    <div className='container'>
+      <Navbar />
+      <div className="row">
+        <ToastContainer
+          position="bottom-left" // Position of the notifications (top-right, top-left, bottom-right, bottom-left)
+          autoClose={2000} // Auto close the notifications after the specified time in milliseconds
+        />        <div className="d-none d-lg-block col-lg-2 p-0">
+          <Sidebar />
+        </div><div className="col-lg-10">
+          <Routes>
+            <Route path='/' element={<Inbox />} />
+            <Route path='/favorites' element={<Favorites />} />
+            <Route path='/sent' element={<Sent />} />
+            <Route path='/spam' element={<Spam />} />
+            <Route path='/trash' element={<Trash />} />
+            <Route path='/message-view/:id' element={<MessageView />} />
+            <Route path='/profile/:id' element={<Profile />} />
+            <Route path='/create-message/:id' element={<CreateMessage />} />
+            <Route path='/create-message' element={<CreateMessage />} />
+          </Routes>
+        </div>
       </div>
-    </BrowserRouter>
+
+
+
+      <CustomButton id={id} />
+    </div>
 
   )
 }
