78651a92b8ee300ac725024b36e369c197a764d0
Chazz Carrizales <czcarrizales@yahoo.com>
2023-07-28 18:46:48 -0700

    wrapping App with providers and themes

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,22 @@ import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { InputProvider } from './InputContext.tsx'
+import { MessageProvider } from './MessageContext.tsx'
+import { SkeletonTheme } from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <MessageProvider>
+      <InputProvider>
+      <SkeletonTheme baseColor='#313131' highlightColor='#525252'>
+      <BrowserRouter>
     <App />
+    </BrowserRouter>
+    </SkeletonTheme>
+      </InputProvider>
+    </MessageProvider>
   </React.StrictMode>,
 )
