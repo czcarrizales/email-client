8394f003439cd280d663965c3321c15865df8b61
Chazz Carrizales <czcarrizales@yahoo.com>
2023-07-28 19:07:10 -0700

    fix errors

--- src/CardSkeleton.tsx
+++ src/CardSkeleton.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import Skeleton from 'react-loading-skeleton'
 import './CardSkeleton.css'
+import React from 'react'
 
 const CardSkeleton = ({ cards }) => {
     return (
-        Array(cards).fill(0).map(item => (
+        Array(cards).fill(0).map(_item => (
             <div className='card-skeleton row d-flex align-items-center justify-content-center'>
                 <div className="col-3 d-flex align-items-center justify-content-center">
                     <Skeleton width={60} height={60} />
