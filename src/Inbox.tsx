632926fde7a5227b3180dfc4d7e19656f661e6bd
Chazz Carrizales <czcarrizales@yahoo.com>
2023-07-28 18:46:35 -0700

    adding filter and notifications for Inbox

--- src/Inbox.tsx
+++ src/Inbox.tsx
@@ -1,26 +1,53 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Message from './Message'
+import axios from 'axios'
+import { MessageContext } from './MessageContext'
+import { InputContext } from './InputContext'
+import CardSkeleton from './CardSkeleton'
 
 const Inbox = () => {
 
-    const getMessages = () => {
-        return (
-          <div>
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-          </div>
-        )
+    const {messages, notification, setNotification, loadingMessages, setLoadingMessages} = useContext(MessageContext)
+    const {inputValue} = useContext(InputContext)
+    const [filteredMessages, setFilteredMessages] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        const filtered = messages.filter((message) => {
+            const first = message.name.first
+            return first.toLowerCase().includes(inputValue.toLowerCase())
+        })
+        setFilteredMessages(filtered)
+    }, [messages, inputValue])
+
+    useEffect(() => {
+        console.log('notification changed!')
+        if (notification) {
+            setTimeout(() => {
+                setNotification(false)
+                console.log('notification is now false')
+            }, 2000)
+        }
+        
+    }, [notification])
+
+
+      const displayMessages = () => {
+        return filteredMessages.map((message) => {
+            if(message.trash || message.spam) {
+                
+            } else {
+                return (<Message firstName={message.name.first} lastName={message.name.last} picture={message.picture.medium} id={message.login.uuid} />)
+            }
+        } )
       }
   return (
-    <div className="row">
-        {getMessages()}
+    <div className='row'>
+        <div className="col-12">
+        {loadingMessages && <CardSkeleton cards={10} />}
+        {!loadingMessages && displayMessages()}
+        </div>
+        
     </div>
   )
 }
