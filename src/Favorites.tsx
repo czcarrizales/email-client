6f81193a6d097498a6bb3076cd61aab2ca2793df
Chazz Carrizales <czcarrizales@yahoo.com>
2023-07-28 18:46:15 -0700

    displaying messages on Favorites

--- src/Favorites.tsx
+++ src/Favorites.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { InputContext } from './InputContext'
+import Message from './Message'
+import { MessageContext } from './MessageContext'
 
 const Favorites = () => {
+    const {messages} = useContext(MessageContext)
+    const favoritedMessages = messages.filter((message) => message.favorited)
+    const {inputValue} = useContext(InputContext)
+    const [filteredMessages, setFilteredMessages] = useState([])
+
+    useEffect(() => {
+        const filtered = favoritedMessages.filter((message) => {
+            const first = message.name.first
+            return first.toLowerCase().includes(inputValue.toLowerCase())
+        })
+        setFilteredMessages(filtered)
+        console.log(filteredMessages, inputValue)
+    }, [messages, inputValue])
+
+
+      const displayMessages = () => {
+        return filteredMessages.map((message) => (<Message firstName={message.name.first} lastName={message.name.last} picture={message.picture.medium} id={message.login.uuid} />))
+      }
   return (
-    <div>Favorites</div>
+    <div>
+        {displayMessages()}
+    </div>
   )
 }
 
